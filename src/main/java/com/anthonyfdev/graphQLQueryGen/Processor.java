9515eed29f159f5dcb0f974b33cc3ece0617720a
Anthony Fermin <afermin@fuzzproductions.com>
2016-11-09 16:02:25 -0500

    cleaned up some magic constants

--- src/main/java/com/anthonyfdev/graphQLQueryGen/Processor.java
+++ src/main/java/com/anthonyfdev/graphQLQueryGen/Processor.java
@@ -22,13 +22,15 @@ import java.util.Set;
 @SupportedSourceVersion(SourceVersion.RELEASE_7)
 public class Processor extends AbstractProcessor {
 
-    private static final String MODELS_PACKAGE = "com.anthonyfdev.graphQLQueryGen.models";
+    private static final String PACKAGE_MODELS = "com.anthonyfdev.graphQLQueryGen.models";
+    private static final String METHOD_GET_QUERY = "getQuery";
+    private static final String MODEL_CLASS_PREFIX = "GraphQL_";
 
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         Set<Element> graphQLObjectSet = (Set<Element>) roundEnv.getElementsAnnotatedWith(GraphQLObject.class);
         for (Element modelElement : graphQLObjectSet) {
             //building the getQuery() method
-            MethodSpec.Builder mb = MethodSpec.methodBuilder("getQuery")
+            MethodSpec.Builder mb = MethodSpec.methodBuilder(METHOD_GET_QUERY)
                     .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
                     .returns(String.class)
                     .addStatement("$T sb = new StringBuilder()", StringBuilder.class)
@@ -38,11 +40,10 @@ public class Processor extends AbstractProcessor {
                 if (element.getKind().isField()
                         && element.getAnnotation(GraphQLField.class) != null) {
                     String fieldName = element.getSimpleName().toString();
-
                     mb.addStatement("sb.append(\" $L \")", fieldName);
                     if (!isScalarType(element)) {
-                        mb.addStatement("sb.append(com.anthonyfdev.graphQLQueryGen.models.GraphQL_$L.getQuery())"
-                                , getFieldType(element));
+                        mb.addStatement("sb.append($L.$L$L.$L())",
+                                PACKAGE_MODELS, MODEL_CLASS_PREFIX, getFieldType(element), METHOD_GET_QUERY);
                     }
                 }
             }
@@ -50,19 +51,19 @@ public class Processor extends AbstractProcessor {
                     .addStatement("return sb.toString()");
 
             //building class
-            String className = "GraphQL_" + getClassName(modelElement);
+            String className = MODEL_CLASS_PREFIX + getClassName(modelElement);
             TypeSpec typeSpec = TypeSpec.classBuilder(className)
                     .addModifiers(Modifier.PUBLIC)
                     .addMethod(mb.build())
                     .build();
 
             //building class file
-            JavaFile javaFile = JavaFile.builder(MODELS_PACKAGE, typeSpec)
+            JavaFile javaFile = JavaFile.builder(PACKAGE_MODELS, typeSpec)
                     .build();
 
             //writing the file
             try {
-                JavaFileObject source = processingEnv.getFiler().createSourceFile(MODELS_PACKAGE + "." + className);
+                JavaFileObject source = processingEnv.getFiler().createSourceFile(PACKAGE_MODELS + "." + className);
 
                 Writer writer = source.openWriter();
                 writer.write(javaFile.toString());
