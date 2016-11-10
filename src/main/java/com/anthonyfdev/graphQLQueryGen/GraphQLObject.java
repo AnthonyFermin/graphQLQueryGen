e6ef97a9c735984b52f90d2d8f2284aa7e9701ef
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2016-11-10 16:26:13 -0500

    Add some cool javadoc to the annotation classes

--- src/main/java/com/anthonyfdev/graphQLQueryGen/GraphQLObject.java
+++ src/main/java/com/anthonyfdev/graphQLQueryGen/GraphQLObject.java
@@ -5,6 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marker interface for classes whose fields should be serialized
+ * into a GraphQL query.
+ *
+ * @see GraphQLField
+ */
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.TYPE)
 public @interface GraphQLObject {
